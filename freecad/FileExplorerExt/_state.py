72ae45d1eaf4fc837e30a463c4bb80c37fa969a5
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-12-23 22:54:12 -0500

    Add SPDX File Notices

--- freecad/FileExplorerExt/_state.py
+++ freecad/FileExplorerExt/_state.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # SPDX-FileCopyrightText: 2025 Frank David Martínez Muñoz
+# SPDX-FileNotice: Part of the File Explorer addon.
 
 """
 FileExplorerExt: Persistent state.
