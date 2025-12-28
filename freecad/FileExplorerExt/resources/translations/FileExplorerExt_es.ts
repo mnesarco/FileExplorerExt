9e3bf7c88525672ad614fa89d90de81eca1f9c78
Frank Martinez <mnesarco@gmail.com>
2025-12-28 13:46:59 -0500

    [intl] add translations (es)

---
+++ freecad/FileExplorerExt/resources/translations/FileExplorerExt_es.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name></name>
+    <message>
+        <location filename="../../_commands.py" line="28"/>
+        <source>FileExplorerExt</source>
+        <comment>Toggle File Explorer</comment>
+        <translation>Explorador de archivos</translation>
+    </message>
+    <message>
+        <location filename="../../_commands.py" line="29"/>
+        <source>FileExplorerExt</source>
+        <comment>Toggle File Explorer Panel</comment>
+        <translation>Visibilidad del panel de explorador de archivos</translation>
+    </message>
+    <message>
+        <location filename="../../_commands.py" line="56"/>
+        <source>FileExplorerExt</source>
+        <comment>Toggle File Explorer View</comment>
+        <translation>Visibilidad del panel de explorador de archivos</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="64"/>
+        <source>FileExplorerExt</source>
+        <comment>Back</comment>
+        <translation>Atrás</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="69"/>
+        <source>FileExplorerExt</source>
+        <comment>Forward</comment>
+        <translation>Adelante</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="74"/>
+        <source>FileExplorerExt</source>
+        <comment>Up</comment>
+        <translation>Subir un nivel</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="79"/>
+        <source>FileExplorerExt</source>
+        <comment>Filter...</comment>
+        <translation>Filtro...</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="92"/>
+        <location filename="../../_explorer.py" line="106"/>
+        <source>FileExplorerExt</source>
+        <comment>Read only</comment>
+        <translation>Solo lectura</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="103"/>
+        <source>FileExplorerExt</source>
+        <comment>ro</comment>
+        <translation>ro</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="103"/>
+        <source>FileExplorerExt</source>
+        <comment>rw</comment>
+        <translation>rw</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="108"/>
+        <source>FileExplorerExt</source>
+        <comment>Read/Write</comment>
+        <translation>Lectura/Escritura</translation>
+    </message>
+    <message>
+        <location filename="../../_explorer.py" line="144"/>
+        <location filename="../../_favorites.py" line="296"/>
+        <source>FileExplorerExt</source>
+        <comment>File Explorer</comment>
+        <translation>Explorador de archivos</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="36"/>
+        <source>FileExplorerExt</source>
+        <comment>This PC</comment>
+        <translation>Este PC</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="43"/>
+        <source>FileExplorerExt</source>
+        <comment>Home</comment>
+        <translation>Usuario</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="50"/>
+        <source>FileExplorerExt</source>
+        <comment>Macros</comment>
+        <translation>Macros</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="232"/>
+        <source>FileExplorerExt</source>
+        <comment>Duplicated</comment>
+        <translation>Duplicado</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="233"/>
+        <source>FileExplorerExt</source>
+        <comment>Duplicated favorite</comment>
+        <translation>Favorito duplicado</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="249"/>
+        <source>FileExplorerExt</source>
+        <comment>Rename</comment>
+        <translation>Renombrar</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="254"/>
+        <source>FileExplorerExt</source>
+        <comment>Remove from Favorites</comment>
+        <translation>Remover favorito</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="260"/>
+        <location filename="../../_tree.py" line="130"/>
+        <source>FileExplorerExt</source>
+        <comment>Set as default dir</comment>
+        <translation>Establecer como directorio por defecto</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="283"/>
+        <source>FileExplorerExt</source>
+        <comment>Rename Favorite</comment>
+        <translation>Renombrar favorito</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="284"/>
+        <source>FileExplorerExt</source>
+        <comment>Enter new name:</comment>
+        <translation>Nuevo nombre:</translation>
+    </message>
+    <message>
+        <location filename="../../_favorites.py" line="297"/>
+        <source>FileExplorerExt</source>
+        <comment>Cannot rename favorite</comment>
+        <translation>No se pudo renombrar el favorito</translation>
+    </message>
+    <message>
+        <location filename="../../_state.py" line="60"/>
+        <source>FileExplorerExt</source>
+        <comment>File {} could not be imported</comment>
+        <translation>El archivo {} no se pudo importar</translation>
+    </message>
+    <message>
+        <location filename="../../_state.py" line="69"/>
+        <source>FileExplorerExt</source>
+        <comment>File {} could not be opened</comment>
+        <translation>El archivo {} no se pudo abrir</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="98"/>
+        <source>FileExplorerExt</source>
+        <comment>Open</comment>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="105"/>
+        <source>FileExplorerExt</source>
+        <comment>Import into current document</comment>
+        <translation>Importar en el documento actual</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="112"/>
+        <source>FileExplorerExt</source>
+        <comment>Open with Default App</comment>
+        <translation>Abrir con la aplicación por defecto</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="118"/>
+        <source>FileExplorerExt</source>
+        <comment>Copy Path</comment>
+        <translation>Copiar la ruta</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="125"/>
+        <source>FileExplorerExt</source>
+        <comment>Browse</comment>
+        <translation>Explorar en navegador de archivos externo</translation>
+    </message>
+    <message>
+        <location filename="../../_tree.py" line="136"/>
+        <source>FileExplorerExt</source>
+        <comment>Duplicate</comment>
+        <translation>Duplicar archivo</translation>
+    </message>
+</context>
+</TS>
